848cc67c0b0f7757c68d16d04c7c266fe6c0a244
mazhensai <xiao3333ma@gmail.com>
2019-07-03 21:04:05 +0800

    init commit

--- Friday/AppDelegate.swift
+++ Friday/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window!.backgroundColor = UIColor.red;
+        window?.rootViewController = XMBaseNavigationController.init(rootViewController: XMHomeViewController())
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
--- Friday/Business/XMHomeViewController.swift
+++ Friday/Business/XMHomeViewController.swift
@@ -10,4 +10,22 @@ import UIKit
 
 class XMHomeViewController: XMBaseViewController {
 
+    private lazy var button: UIButton =  {
+        let button = UIButton()
+        button.backgroundColor = UIColor.gray
+        button.layer.cornerRadius = 10
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        button.addTarget(self, action: #selector(XMHomeViewController.test), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        title = "Friday"
+        view.backgroundColor = .red
+        view.addSubview(button)
+    }
+    
+    @objc func test() -> Void {
+        print("ddddd")
+    }
 }
