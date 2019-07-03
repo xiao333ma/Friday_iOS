50d2d0e0f666288d6e853fefe340c14dda74107b
mazhensai <xiao3333ma@gmail.com>
2019-07-03 21:03:34 +0800

    first commit

---
+++ Friday/Business/XMHomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  XMHomeViewController.swift
+//  Friday
+//
+//  Created by xiaoma on 2019/7/3.
+//  Copyright © 2019 xiaoma. All rights reserved.
+//
+
+import UIKit
+
+class XMHomeViewController: XMBaseViewController {
+
+}
---
+++ Friday/XMBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XMBaseViewController.swift
+//  Friday
+//
+//  Created by xiaoma on 2019/7/3.
+//  Copyright © 2019 xiaoma. All rights reserved.
+//
+
+import UIKit
+
+class XMBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
